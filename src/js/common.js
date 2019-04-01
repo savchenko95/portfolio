1b847f2be97865768ea72c6b5daae382b414339a
Savchenko Stanislav <48084488+savchenko95@users.noreply.github.com>
2019-04-01 16:55:49 +0300

    added smooth scrolling

--- src/js/common.js
+++ src/js/common.js
@@ -1,2 +1,17 @@
 
 var $ = require("jquery");
+
+// Scroll----------------------
+
+$(document).ready(function(){
+    $("a[href*=\\#]").on("click", function(e){
+        var anchor = $(this);
+        $('html, body').stop().animate({
+            scrollTop: $(anchor.attr('href')).offset().top
+        }, 1000);
+        e.preventDefault();
+        return false;
+    });
+});
+
+//------------------------------
