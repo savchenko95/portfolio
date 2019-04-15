467c10cd0b35fee3090166ae7a59ce5a22416a2b
Savchenko Stanislav <48084488+savchenko95@users.noreply.github.com>
2019-04-15 16:46:03 +0300

    some minimal fixes of contact form

--- src/js/common.js
+++ src/js/common.js
@@ -5,7 +5,7 @@ var $ = require("jquery");
 
 $(document).ready(function(){
   // Scroll----------------------
-    $("a[href*=\\#]").on("click", function(e){
+    $("a[href*=\\#]").on('click', function(e){
         var anchor = $(this);
         $('html, body').stop().animate({
             scrollTop: $(anchor.attr('href')).offset().top
@@ -15,21 +15,27 @@ $(document).ready(function(){
     });
   //------------------------------
   // Modal-window-----------------
-    $( ".project-card" ).on("click", function(){
-      var open = $(this).parent().find(".modal");
-      open.addClass("modal--active");
+    $( '.project-card' ).on('click', function(){
+      var open = $(this).parent().find('.modal');
+      open.addClass('modal--active');
 
-      var close = $(this).parent().find(".modal-close");
-      close.on("click", function(){
-          $(".modal").removeClass("modal--active");
+      var close = $(this).parent().find('.modal-close');
+      close.on('click', function(){
+          $('.modal').removeClass('modal--active');
       });
     });
   //------------------------------
   // Fade Out form after submit---
     var submitted=false;
     $('#gform').on('submit', function(e) {
-      $('#gform *').fadeOut(1500);
-      $('#gform').prepend('Message sent...');
+      $('#gform *').hide(700);
+      $('contact').height(300);
+      setTimeout(
+        function() {
+          $('#gform').addClass('gform--hidden').append('Message sent...')
+        }, 400
+      );
+
     });
   //------------------------------
 });
