ccece07de7347e02864768459480f1232534c66d
Savchenko Stanislav <48084488+savchenko95@users.noreply.github.com>
2019-04-16 13:04:07 +0300

    fix submit function // small style fixes

--- src/js/common.js
+++ src/js/common.js
@@ -1,6 +1,4 @@
-
 var $ = require("jquery");
-
 
 
 $(document).ready(function(){
@@ -18,10 +16,16 @@ $(document).ready(function(){
     $( '.project-card' ).on('click', function(){
       var open = $(this).parent().find('.modal');
       open.addClass('modal--active');
+      $('.modal-grid-container').css('transform', 'translate(-50%,-50%) scale(1)');
 
       var close = $(this).parent().find('.modal-close');
       close.on('click', function(){
-          $('.modal').removeClass('modal--active');
+          $('.modal-grid-container').css('transform', 'translate(-45%,-45%) scale(.1)');
+          setTimeout(
+            function() {
+              $('.modal').removeClass('modal--active');
+            }, 200
+          );
       });
     });
   //------------------------------
