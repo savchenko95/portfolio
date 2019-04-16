4434c96a094a38599c674d5bd76fe413554f1da2
Savchenko Stanislav <48084488+savchenko95@users.noreply.github.com>
2019-04-16 13:04:38 +0300

    added go top button

--- src/js/common.js
+++ src/js/common.js
@@ -42,4 +42,29 @@ $(document).ready(function(){
 
     });
   //------------------------------
+  // Go top arrow-----------------
+  $('body').append(`
+    <nav id="go-top">
+      <svg class="svg-icon-arrow">
+        <use id="up-arrow" class="arrow" href="/static/img/icons/up_arrow.svg#Capa_1"></use>
+      </svg>
+    </nav>`);
+  });
+
+  $(function() {
+    $.fn.scrollToTop = function() {
+      $(this).hide().removeAttr("href");
+      if ($(window).scrollTop() >= "500") $(this).fadeIn(500)
+      var scrollDiv = $(this);
+      $(window).scroll(function() {
+        if ($(window).scrollTop() <= "500") $(scrollDiv).fadeOut(300)
+        else $(scrollDiv).fadeIn("slow")
+      });
+      $(this).click(function() {
+        $("html, body").animate({scrollTop: 0}, "slow")
+      })
+    }
+    $(function() {
+      $("#go-top").scrollToTop();
+    });
 });
